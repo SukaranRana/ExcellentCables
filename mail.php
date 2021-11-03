6e243cc932c3aae67770a1d7da8767636ae966b2
Sukaran Rana <sukaranrana.rana@gmail.com>
2021-11-03 10:37:55 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,15 @@
 <?php
-if(isset($_POST['email'])){
     $admin_email="sukaranrana.rana@gmail.com";
     $name=$_POST['name'];
     $mobile=$_POST['mobile'];
     $email=$_POST['email'];
     $quantity=$_POST['quantity'];
     $message=$_POST['message'];
-
-    mail($admin_email,"New Customer Message",$message . '-' . $mobile, "From:" . $email);
+    
+    if($email!=NULL){
+        mail($admin_email,"New Customer Message",$message . '-' . $mobile, "From:" . $email);
+    }
 
     header('Location: http://www.google.com');
        
-}
 ?>
